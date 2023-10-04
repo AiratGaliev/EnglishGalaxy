ad4f0b908ef3f9080545383c79eec03a80ba0eef
Airat Galiev <airat.galiev@gmail.com>
2023-10-04 17:01:38 +0600

    update packages

--- start.bat
+++ start.bat
@@ -1,2 +1,19 @@
-call venv\Scripts\activate.bat
+@echo off
+
+if not exist venv (
+    echo Creating venv...
+    python -m venv venv
+    call venv\Scripts\activate
+    echo Installing dependencies...
+    pip install --upgrade pip setuptools wheel
+    pip install -r requirements.txt
+    if errorlevel 1 (
+        echo Error happens. Deleting venv...
+        rmdir /s /q venv
+        exit
+    )
+) else (
+    call venv\Scripts\activate
+)
 python main.py
+deactivate
--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-source venv/bin/activate
+
+if [ ! -d "venv" ]; then
+    echo "Creating venv..."
+    python -m venv venv
+    source venv/bin/activate
+    echo "Installing dependencies..."
+    pip install -r requirements.txt
+    if [ $? -ne 0 ]; then
+        echo "Error happens. Deleting venv..."
+        rm -rf venv
+        exit 1
+    fi
+else
+    source venv/bin/activate
+fi
 python main.py
+deactivate
