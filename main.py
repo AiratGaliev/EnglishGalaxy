bbf4dc93e4884ed3dec7d757181ed3c9a5971eb3
Airat Galiev <airat.galiev@gmail.com>
2023-10-12 02:03:23 +0600

    fix logic

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
     password = Config.PASSWORD.value
     lessons_id: list[int] = parse_numeric_array("1..50")
     regenerate_exercise_id: int = 0
-    is_not_invalid: bool = True
     generate_all_level: bool = True if input('Generate all levels? y/n (default n):') == 'y' else False
     generate_all_lessons_text: bool = True if input('Generate all lessons text? y/n (default n):') == 'y' else False
     regenerate_all_lessons: bool = True if input('Regenerate all lesson? y/n (default n):') == 'y' else False
@@ -23,23 +22,19 @@ if __name__ == '__main__':
     else:
         checked_levels.append(levels[int(level_id)]) if level_id != '' else checked_levels.extend(levels)
     for level in checked_levels:
-        while is_not_invalid:
-            try:
-                print("Lessons level is {level}".format(level=level.upper()))
-                if american_accent:
-                    print("An American accent has been chosen")
-                if british_accent:
-                    print("A British accent has been chosen")
-                if not (generate_all_lessons_text or regenerate_all_lessons):
-                    lessons_str = input("Enter generate lessons in the format '1' or '1..50'(default):")
-                    if lessons_str != '':
-                        lessons_id = parse_numeric_array(lessons_str)
-                        regenerate_exercise_str = input('Regenerate exercise id:')
-                        regenerate_exercise_id = int(regenerate_exercise_str)
-                is_not_invalid = False
-            except ValueError:
-                is_not_invalid = True
-        is_not_invalid = True
+        try:
+            print("Lessons level is {level}".format(level=level.upper()))
+            if american_accent:
+                print("An American accent has been chosen")
+            if british_accent:
+                print("A British accent has been chosen")
+            if not (generate_all_lessons_text or regenerate_all_lessons):
+                lessons_str = input("Enter generate lessons in the format '1' or '1..50'(default):")
+                if lessons_str != '':
+                    lessons_id = parse_numeric_array(lessons_str)
+                    regenerate_exercise_id = int(input('Regenerate exercise id:'))
+        except ValueError:
+            input('Not a number')
         for lesson_id in lessons_id:
             generate_cards(level, lesson_id, regenerate_exercise_id, regenerate_all_lessons, american_accent,
                            british_accent, collection_media, documents, access_token)
