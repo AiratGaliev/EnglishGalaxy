630cd5f0829b2dce52fba3a0849290c0c943185e
Airat Galiev <airat.galiev@gmail.com>
2024-01-27 17:29:03 +0600

    update scripts files

--- start_cli.sh
+++ start_cli.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ ! -d "venv" ]; then
-    echo "Creating venv..."
-    python3 -m venv venv
-    source venv/bin/activate
+if [ ! -d ".venv" ]; then
+    echo "Creating .venv directory..."
+    python3 -m venv .venv
+    source .venv/bin/activate
     echo "Installing dependencies..."
     pip install --upgrade pip setuptools wheel
     pip install -r requirements.txt
     if [ $? -ne 0 ]; then
-        echo "Error happens. Deleting venv..."
-        rm -rf venv
+        echo "Error happens. Deleting .venv directory..."
+        rm -rf .venv
         exit 1
     fi
 else
-    source venv/bin/activate
+    source .venv/bin/activate
 fi
 python3 start_cli.py
 deactivate
